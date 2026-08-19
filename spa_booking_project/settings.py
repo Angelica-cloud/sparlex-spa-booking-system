7ff56c1bc05e65e1510e473bf02588e7ff705629
Angel Isiogu <angelisiogu@gmail.com>
2026-08-18 22:40:00 -0700

    Configure static files for Render

--- spa_booking_project/settings.py
+++ spa_booking_project/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "bookings/static"
 ]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
